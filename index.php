83925caa716ea4f770a454a468864fa07fc7689b
unknown <alexander.baikalov@inslapineda.cat>
2022-09-27 09:57:45 +0100

    styling

--- index.php
+++ index.php
@@ -1,36 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/admin.css">
+    <link rel="stylesheet" href="./styles/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Document</title>
 </head>
+
 <body>
-<h1>Inici de sessió</h1>
 
-<div style="display:flex;justify-content: center; align-items: center;">
+    <img id="imgAjuntament" src="./assets/ajuntament.png" alt="ajuntament de badalona" />
+    <h1 id="rainbow-title">InfoBDN</h1>
 
-<p><a href="./admin/adminLogin.php"  class="btn btn-primary">Admin</a></p>
-<p><a href="login.php"  class="btn btn-primary">Estudiant</a></p>
-<p><a href="login.php"  class="btn btn-primary">Professor</a></p>
+    <br />
+    <br />
+    <div id="content">
+    <h3>Portal inici de sessió</h3>
 
+        <div id="loginBox">
+            <p><a href="student/studentLogin.php" class="btn btn-primary">Estudiant</a></p>
+            <p><a href="login.php" class="btn btn-primary">Professor</a></p>
+        </div>
+    </div>
+
+    <div id="bottom">
+    <a href="./admin/adminLogin.php">Admin</a>
+    <a href="./admin/adminLogin.php">Acerca de</a>
+    <p>© 2021 INS La Pineda</p>
+    </div>
+
+</body>
 <?php
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'courses');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
-</div>
-</body>
+
 </html>
